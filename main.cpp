0e228de3a1848e6a6bb447550227d0bd38ac9d73
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-04-09 13:24:35 -0600

    basic functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <fstream>
+
+const std::string file_name = "file.txt";
+const std::string key_word = "* Horas:";
+const std::string units = "hrs";
+
+std::ifstream file;
+int total_values;
+
+void initialize_variables()
+{
+    file.open(file_name);
+    total_values = 0;
+}
+
+void get_and_search_file_content()
+{
+    std::string line;
+    while (getline(file, line))
+    {
+        if (line.front() == key_word[0])
+        {
+            std::size_t position = line.find_last_of(key_word);
+            std::cout << line[position] << '\n';
+        }
+    }
+}
+
+void show_total_values()
+{
+    std::cout << "Total " << units << ": " << total_values << '\n';
+}
 
 int main()
 {
+    initialize_variables();
+    get_and_search_file_content();
     return 0;
 }
